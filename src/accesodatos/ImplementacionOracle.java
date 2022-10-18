110854a6a5f2b89cb0080a6d468e1ac018d90be4
lizethdd <keeylic@outlook.es>
2022-10-18 12:25:59 +0200

    Done

---
+++ src/accesodatos/ImplementacionOracle.java
@@ -0,0 +1,26 @@
+
+package accesodatos;
+
+
+public class ImplementacionOracle implements IAccesoDatos{
+      @Override
+    public void insertar() {
+        System.out.println("Insertar desde Mysql");
+    }
+
+    @Override
+    public void listar() {
+        System.out.println("Listar desde Mysql");
+    }
+
+    @Override
+    public void actualizar() {
+        System.out.println("Actualizar desde Mysql");
+    }
+
+    @Override
+    public void eliminar() {
+        System.out.println("Eliminar desde Mysql");
+    }
+    
+}
